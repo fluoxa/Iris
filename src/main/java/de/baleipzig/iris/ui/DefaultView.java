336a1b957bd7d580186972be98381d42a95cbca8
fluoxa <fluoxa@googlemail.com>
2016-10-07 16:44:47 +0200

    Spring Vaadin komplett

---
+++ src/main/java/de/baleipzig/iris/ui/DefaultView.java
@@ -0,0 +1,26 @@
+package de.baleipzig.iris.ui;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+import javax.annotation.PostConstruct;
+
+@UIScope
+@SpringView(name = DefaultView.VIEW_NAME)
+public class DefaultView extends VerticalLayout implements View {
+    public static final String VIEW_NAME = "";
+
+    @PostConstruct
+    void init() {
+        addComponent(new Label("This is the default view"));
+    }
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+        // the view is constructed in the init() method()
+    }
+}
---
+++ src/main/resources/VAADIN/widgetsets/de.baleipzig.iris.IrisWidgetset/deferredjs/F58448541E191BEA7FB1695BA83FB574/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.de_baleipzig_iris_IrisWidgetset.runAsyncCallback2('Xdb(1611,1,n2d);_.vc=function Lic(){y3b((!r3b&&(r3b=new D3b),r3b),this.a.d)};KXd(Th)(2);\n//# sourceURL=de.baleipzig.iris.IrisWidgetset-2.js\n')
