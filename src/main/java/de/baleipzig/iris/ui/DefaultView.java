00665a5170a1722b9cd2476eaadef2e09c5f527e
fluoxa <fluoxa@googlemail.com>
2016-10-11 16:01:57 +0200

    Machbarkeit - Zeichnen im Browser

--- src/main/java/de/baleipzig/iris/ui/DefaultView.java
+++ src/main/java/de/baleipzig/iris/ui/DefaultView.java
@@ -15,6 +15,7 @@ import de.baleipzig.iris.persistence.entity.neuralnet.NeuralNetEntity;
 import de.baleipzig.iris.persistence.entity.neuralnet.NodeEntity;
 import de.baleipzig.iris.persistence.repository.NeuralNetEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.hezamu.canvas.Canvas;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -27,12 +28,60 @@ public class DefaultView extends VerticalLayout implements View {
     @Autowired
     NeuralNetEntityRepository repository;
 
+    private boolean mouseDown = false;
+    private int previousX;
+    private int previousY;
+
     @PostConstruct
     void init() {
         Button button = new Button("create Test entity");
         button.addClickListener(clickEvent -> createTestEntity());
         addComponent(new Label("This is the default view"));
         addComponent(button);
+
+        Label label = new Label("klicken und Maus bewegen im Rahmen :)");
+        addComponent(label);
+
+        Canvas canvas = new Canvas();
+        canvas.setWidth("300px");
+        canvas.setHeight("300px");
+        canvas.setStrokeStyle(0,0,0);
+        canvas.setLineWidth(10);
+        canvas.setLineCap("round");
+        canvas.addStyleName("iris-canvas");
+        canvas.addMouseMoveListener(mouseEventDetails -> {
+            int x = mouseEventDetails.getRelativeX();
+            int y = mouseEventDetails.getRelativeY();
+
+            System.out.println(x + "/" + y);
+
+            if(mouseDown) {
+                canvas.beginPath();
+                canvas.moveTo(previousX, previousY);
+                canvas.lineTo(x, y);
+                canvas.stroke();
+                canvas.closePath();
+            }
+            previousX = x;
+            previousY = y;
+        });
+
+        canvas.addMouseDownListener(() -> {
+            System.out.println("clicked");
+            mouseDown = true;
+
+        });
+
+
+        canvas.addMouseUpListener(() -> {
+            System.out.println("released");
+            mouseDown = false;
+        });
+
+        addComponent(canvas);
+
+
+
     }
 
     private void createTestEntity() {
