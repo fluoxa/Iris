943b0f23594d3211c63f4e5d1ca444a0556a59bf
fluoxa <fluoxa@googlemail.com>
2016-10-12 10:14:34 +0200

    Entity end Repo für Bilder

--- src/main/java/de/baleipzig/iris/ui/DefaultView.java
+++ src/main/java/de/baleipzig/iris/ui/DefaultView.java
@@ -4,13 +4,8 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
-import de.baleipzig.iris.logic.worker.INeuralNetWorker;
-import de.baleipzig.iris.persistence.repository.INeuralNetEntityRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.vaadin.hezamu.canvas.Canvas;
 
 import javax.annotation.PostConstruct;
 
@@ -19,66 +14,9 @@ import javax.annotation.PostConstruct;
 public class DefaultView extends VerticalLayout implements View {
     public static final String VIEW_NAME = "";
 
-    @Autowired
-    INeuralNetEntityRepository repository;
-
-    @Autowired
-    INeuralNetWorker neuralNetWorker;
-
-    private boolean mouseDown = false;
-    private int previousX;
-    private int previousY;
-
     @PostConstruct
     void init() {
-        Button button = new Button("create Test entity");
-        button.addClickListener(clickEvent -> createTestEntity());
-        addComponent(new Label("This is the default view"));
-        addComponent(button);
-
-        Label label = new Label("klicken und Maus bewegen im Rahmen :)");
-        addComponent(label);
-
-        Canvas canvas = new Canvas();
-        canvas.setWidth("300px");
-        canvas.setHeight("300px");
-        canvas.setStrokeStyle(0,0,0);
-        canvas.setLineWidth(10);
-        canvas.setLineCap("round");
-        canvas.addStyleName("iris-canvas");
-        canvas.addMouseMoveListener(mouseEventDetails -> {
-            int x = mouseEventDetails.getRelativeX();
-            int y = mouseEventDetails.getRelativeY();
-
-            System.out.println(x + "/" + y);
-
-            if(mouseDown) {
-                canvas.beginPath();
-                canvas.moveTo(previousX, previousY);
-                canvas.lineTo(x, y);
-                canvas.stroke();
-                canvas.closePath();
-            }
-            previousX = x;
-            previousY = y;
-        });
-
-        canvas.addMouseDownListener(() -> {
-            System.out.println("clicked");
-            mouseDown = true;
-
-        });
-
-
-        canvas.addMouseUpListener(() -> {
-            System.out.println("released");
-            mouseDown = false;
-        });
-
-        addComponent(canvas);
-
-
-
+        addComponent(new Label("defaultView"));
     }
 
     private void createTestEntity() {
@@ -88,6 +26,5 @@ public class DefaultView extends VerticalLayout implements View {
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
-
     }
 }
