4ecc9b08e8d36b01309abc3235f2eb66989a88c4
fluoxa <fluoxa@googlemail.com>
2016-10-13 15:58:32 +0200

    Bilder Fortsetzung

--- src/main/java/de/baleipzig/iris/ui/playground/DanielView.java
+++ src/main/java/de/baleipzig/iris/ui/playground/DanielView.java
@@ -7,12 +7,15 @@ import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+import de.baleipzig.iris.ImageType;
 import de.baleipzig.iris.logic.worker.IImageWorker;
+import de.baleipzig.iris.model.image.IImage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.hezamu.canvas.Canvas;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @UIScope
 @SpringView(name = DanielView.VIEW_NAME)
@@ -33,12 +36,12 @@ public class DanielView extends VerticalLayout implements View {
         Button button = new Button("test");
 
         button.addClickListener(clickEvent -> {
-            imageWorker.exportImageToDb();
+            //imageWorker.exportImageToDb();
 //            long count = imageWorker.countImagesByType(ImageType.TEST);
 //            count = 0;
             //List<IImage> list =  imageWorker.loadAllImagesByType(ImageType.TRAIN);
-            //List<IImage> list = imageWorker.loadRandomImagesByType(1000, ImageType.TRAIN);
-            //String foo = "";
+            List<IImage> list = imageWorker.loadRandomImagesByType(1000, ImageType.TRAIN);
+            String foo = "";
         });
 
         addComponent(button);
@@ -92,6 +95,4 @@ public class DanielView extends VerticalLayout implements View {
     public void enter(ViewChangeListener.ViewChangeEvent event) {
 
     }
-
-
 }
