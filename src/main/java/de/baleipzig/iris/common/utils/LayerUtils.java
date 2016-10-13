696a5d1a5609a6c3eceaeeab608ec872271f6f85
sonny <unknown>
2016-10-13 08:26:21 +0200

    * Hinzufügen von LayerUtils zum Anlegen und Verknüpfen von Layern

--- src/main/java/de/baleipzig/iris/common/utils/LayerUtils.java
+++ src/main/java/de/baleipzig/iris/common/utils/LayerUtils.java
@@ -1,7 +1,12 @@
 package de.baleipzig.iris.common.utils;
 
+import de.baleipzig.iris.common.Dimension;
+import de.baleipzig.iris.model.neuralnet.axon.Axon;
+import de.baleipzig.iris.model.neuralnet.axon.IAxon;
 import de.baleipzig.iris.model.neuralnet.layer.ILayer;
+import de.baleipzig.iris.model.neuralnet.layer.Layer;
 import de.baleipzig.iris.model.neuralnet.node.INode;
+import de.baleipzig.iris.model.neuralnet.node.Node;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,5 +29,52 @@ public class LayerUtils {
             }
 
         return list;
+    }
+
+    public static ILayer createRandomLayer(Dimension dim, boolean useRandomBias){
+
+        ILayer layer = new Layer();
+        layer.resize(dim);
+
+        for(int i = 0; i < dim.getY(); i++)
+            for (int j = 0; j < dim.getX(); j++){
+
+                INode node = new Node();
+                node.setBias( useRandomBias ? (Math.random()-0.5)*10. : 0.); //todo: range herausfinden
+                layer.addNode(node);
+            }
+
+        return layer;
+    }
+
+    public static void fullyConnectLayers(ILayer parent, ILayer child, boolean useRandomWeights){
+
+        int sizeX = parent.getDim().getX();
+        int sizeY = parent.getDim().getY();
+
+        for(int i = 0; i < sizeY; i++)
+            for(int j = 0; j < sizeX; j++){
+
+                INode tmp = parent.getNode(j,i);
+                LayerUtils.connectNodeChildLayer(tmp, child, useRandomWeights);
+            }
+    }
+
+    private static void connectNodeChildLayer (INode node, ILayer childLayer, boolean useRandomWeights){
+
+        int sizeX = childLayer.getDim().getX();
+        int sizeY = childLayer.getDim().getY();
+
+        for(int i = 0; i < sizeY; i++)
+            for(int j = 0; j < sizeX; j++){
+
+                INode tmp = childLayer.getNode(j,i);
+                IAxon axon = new Axon();
+                axon.setWeight(useRandomWeights ? Math.random()-0.5 : 0); //todo: range herausfinden
+                axon.setParentNode(node);
+                axon.setChildNode(tmp);
+                node.addChildAxon(axon);
+                tmp.addParentAxon(axon);
+            }
     }
 }
