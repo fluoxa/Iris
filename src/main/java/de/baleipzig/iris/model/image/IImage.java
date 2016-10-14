79d32c9e8df9cfde9c4f22f9a741bd408628f231
fluoxa <fluoxa@googlemail.com>
2016-10-14 11:26:33 +0200

    Bilder Review

--- src/main/java/de/baleipzig/iris/model/image/IImage.java
+++ src/main/java/de/baleipzig/iris/model/image/IImage.java
@@ -4,23 +4,17 @@ import de.baleipzig.iris.ImageType;
 
 public interface IImage {
     String getName();
-
     void setName(String name);
 
     ImageType getImageType();
-
     void setImageType(ImageType imageType);
 
     int getDigit();
-
     void setDigit(int digit);
 
     int getImageNumber();
-
     void setImageNumber(int imageNumber);
 
     String getImageAsBase64();
-
     void setImageAsBase64(String imageAsBase64);
-
 }
