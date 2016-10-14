1c2a2c2de70f5856aa126289cdae0a91d454b0d1
fluoxa <fluoxa@googlemail.com>
2016-10-14 12:38:52 +0200

    Bilder Branch nach Master

--- src/test/java/de/baleipzig/iris/logic/converter/neuralnet/DigitConverterTest.java
+++ src/test/java/de/baleipzig/iris/logic/converter/neuralnet/DigitConverterTest.java
@@ -12,24 +12,20 @@ public class DigitConverterTest {
         return new Object[][]{{-1},{10}};
     }
 	
-	@Test(dataProvider="convert_ReturnsStateZeroLayer_WhenNumberOutOfRange")
-	public void convert_ReturnsStateZeroLayer_WhenNumberOutOfRange(int digit){
-	
-		IEntityLayerConverter<Integer> digitConverter = new DigitConverter();
-		
-		ILayer layer = digitConverter.convert(digit);
-		
-		for(int pos = 0; pos < 9; pos++) {
-			Assert.assertEquals(layer.getNode(pos, 0).getState() ,0.);
-		}
-	}
-	
 	@DataProvider(name = "convert_ExpectedResult_WhenDigitIsInRange")
     public static Object[][] convert_data2() {
 		return new Object[][]{
 				{0},{1},{2},{3},{4},
 				{5},{6},{7},{8},{9}};
     }
+
+	@Test(dataProvider = "convert_ReturnsStateZeroLayer_WhenNumberOutOfRange", expectedExceptions = RuntimeException.class)
+	public void convert_ReturnsStateZeroLayer_WhenNumberOutOfRange(int digit) throws RuntimeException {
+
+		IEntityLayerConverter<Integer> digitConverter = new DigitConverter();
+
+		ILayer layer = digitConverter.convert(digit);
+	}
 	
 	@Test(dataProvider="convert_ExpectedResult_WhenDigitIsInRange")
 	public void convert_ExpectedResult_WhenDigitIsInRange(int digit){
