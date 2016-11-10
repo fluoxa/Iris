35c8126af8f68bd1e6558126b489fe94b3d09985
fluoxa <fluoxa@googlemail.com>
2016-11-10 12:26:47 +0100

    reco

--- src/main/java/de/baleipzig/iris/ui/view/recognition/RecognitionView.java
+++ src/main/java/de/baleipzig/iris/ui/view/recognition/RecognitionView.java
@@ -3,7 +3,7 @@ package de.baleipzig.iris.ui.view.recognition;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.*;
 import de.baleipzig.iris.ui.language.LanguageHandler;
 import de.baleipzig.iris.ui.presenter.recognition.RecognitionPresenter;
 import de.baleipzig.iris.ui.service.recognition.IRecognitionService;
@@ -33,9 +33,32 @@ public class RecognitionView extends BaseSearchNNView<RecognitionPresenter> impl
 
     @PostConstruct
     private void init() {
-        setBodyContent(new Label("RecognitionView"));
+        createLayout();
 
         presenter = new RecognitionPresenter(this, (IRecognitionService)  context.getBean("recognitionService"));
         presenter.init();
+    }
+
+    private void createLayout() {
+
+
+        VerticalLayout recognitionLayout = new VerticalLayout();
+
+
+        VerticalLayout additionalInfoLayout = new VerticalLayout();
+        for (int i = 0; i < 5; i++) {
+            additionalInfoLayout.addComponent(new Button(i + ""));
+        }
+
+        Panel additionalInfoPanel = new Panel();
+        additionalInfoPanel.setContent(additionalInfoLayout);
+
+        AbsoluteLayout minimizableLayout = new AbsoluteLayout();
+        minimizableLayout
+
+        HorizontalLayout recognitionMainLayout = new HorizontalLayout();
+
+
+        setBodyContent(new Label("RecognitionView"));
     }
 }
